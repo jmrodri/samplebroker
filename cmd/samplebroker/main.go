14a1cef70eb3bde4024ef496559fa63f1cabb5f1
jesus m. rodriguez <jmrodri@gmail.com>
2018-03-30 00:25:56 -0400

    add registry initialization

--- cmd/samplebroker/main.go
+++ cmd/samplebroker/main.go
@@ -20,6 +20,7 @@ import (
 	"os"
 
 	"github.com/automationbroker/bundle-lib/registries"
+	"github.com/cloudflare/cfssl/log"
 	flags "github.com/jessevdk/go-flags"
 	"github.com/openshift/ansible-service-broker/pkg/app"
 )
@@ -38,13 +39,35 @@ func main() {
 		}
 	}
 
-	// if args.Version {
-	//     fmt.Println(version.Version)
-	//     os.Exit(0)
-	// }
-
 	// To add your custom registries, define an entry in this array.
 	regs := []registries.Registry{}
+
+	c := registries.Config{
+		URL:        "",
+		User:       "",
+		Pass:       "",
+		Org:        "jmrodri",
+		Tag:        "latest",
+		Type:       "file", // bundle-lib registry.go needs to change for this
+		Name:       "foo",
+		Images:     []string{"hello-world-db-apb"},
+		Namespaces: []string{"openshift"},
+		Fail:       false,
+		WhiteList:  []string{},
+		BlackList:  []string{},
+		AuthType:   "",
+		AuthName:   "",
+		Runner:     "",
+	}
+
+	reg, err := registries.NewRegistry(c, "openshift")
+	if err != nil {
+		log.Errorf(
+			"Failed to initialize foo Registry err - %v \n", err)
+		os.Exit(1)
+	}
+
+	regs = append(regs, reg)
 
 	// CreateApp passing in the args and registries
 	app := app.CreateApp(args, regs)
